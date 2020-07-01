80b6f8a2a2a58fb83370771d4f0e1b5cb07ab26b
pavi199226 <pavi199226@gmail.com>
2020-07-01 15:56:39 +0200

    Updates

---
+++ precache-manifest.fb77c83cae77e5ef48bfb1607adebe26.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe325dc79acee97cf2d9f7a3bcaa73f",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "0e26b4a9a1d4a0cc8806",
+    "url": "/project/static/css/main.5286df54.chunk.css"
+  },
+  {
+    "revision": "10ddebac755a411362c2",
+    "url": "/project/static/js/2.1ec8775e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project/static/js/2.1ec8775e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e26b4a9a1d4a0cc8806",
+    "url": "/project/static/js/main.5e49339d.chunk.js"
+  },
+  {
+    "revision": "00779be5a75421897f44",
+    "url": "/project/static/js/runtime-main.102d06f0.js"
+  },
+  {
+    "revision": "6a5e617a4b0e84cc689f7df2153e80ec",
+    "url": "/project/static/media/avataars.6a5e617a.svg"
+  },
+  {
+    "revision": "e27c88e555910ddce50b6f1e275023a4",
+    "url": "/project/static/media/circus.e27c88e5.png"
+  },
+  {
+    "revision": "107d31e81b23d07ad38c7cc4d1f7ea27",
+    "url": "/project/static/media/game.107d31e8.png"
+  },
+  {
+    "revision": "dfa7703b841989c158a12839d3f39e85",
+    "url": "/project/static/media/img2.dfa7703b.jpg"
+  },
+  {
+    "revision": "7dfdcf559d331a0cdc05cc1d2b8c8233",
+    "url": "/project/static/media/safe.7dfdcf55.png"
+  },
+  {
+    "revision": "8fad4ff92040b17345896e0e777a79ad",
+    "url": "/project/static/media/submarine.8fad4ff9.png"
+  },
+  {
+    "revision": "70c4167d809c5f0089c9a8392fbe0b36",
+    "url": "/project/static/media/weather.70c4167d.png"
+  }
+]);
